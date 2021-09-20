3fe30d06f6861add7924dd5332f7ff224dc1aeb0
JesuFemi-O <ogunwedeemmanuel@gmail.com>
2021-09-20 10:56:04 +0100

    chore: disable item resources

--- services/shop_service/src/shops_app/urls.py
+++ services/shop_service/src/shops_app/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('shops/', view=views.ShopList.as_view(), name='shop list'),
     path('shop/<int:shop_id>/',
          view=views.ShopDetail.as_view(), name='shop detail'),
-    path('items/<int:shop_id>/', view=views.ItemList.as_view(), name='shop list'),
-    path('item/<int:shop_id>/<int:item_id>/',
-         view=views.ItemDetail.as_view(), name='item detail')
+#     path('items/<int:shop_id>/', view=views.ItemList.as_view(), name='shop list'),
+#     path('item/<int:shop_id>/<int:item_id>/',
+#          view=views.ItemDetail.as_view(), name='item detail')
 ]
 
 # forcefully update app, again
